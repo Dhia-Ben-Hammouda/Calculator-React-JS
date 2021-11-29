2e4797c7fb6a9c0e2cc61d6217147f9176d80caa
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2021-11-30 00:34:34 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Container from "./Container";
+
+let App =()=>{
+    return(
+        <div className="body">
+            <Container/>
+        </div>
+    );
+}
+
+export default App;
