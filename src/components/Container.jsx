d5c7a0f09184cd4045318b247543851f97f471a4
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2022-04-17 10:22:57 +0200

    add logo

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,73 +1,71 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import "./Container.css";
 
-let Container = ()=>{
-    
-    const [Res , setRes] = useState("");
+let Container = () => {
+
+	const [Res, setRes] = useState("");
+
+	function addChar(event) {
+		if (event.target.innerHTML === "." && Res[Res.length - 1] === ".") {
+			return;
+		}
+		setRes(Res + event.target.innerHTML);
+	}
+
+	function remove() {
+		setRes("");
+	}
+
+	function remove_one() {
+		if (Res.length === 1) {
+			setRes("");
+			return;
+		}
+		setRes(Res.slice(0, Res.length - 1));
+	}
+
+	function calculate() {
+		setRes(eval(Res));
+	}
 
 
-    function addChar(event)
-    {
-        if ( event.target.innerHTML === "." && Res[Res.length-1] === "." )
-        {
-            return;
-        }
-        setRes( Res+event.target.innerHTML );
-    }
+	return (
+		<div className="container">
 
-    function remove()
-    {
-        setRes("");
-    }
-
-    function remove_one()
-    {
-        setRes( Res.slice(0 , Res.length-1)  );
-    }
-
-    function calculate()
-    {
-        setRes( eval(Res)  );
-    }
+			<div className="up">
+				{Res}
+			</div>
 
 
-    return(
-        <div className="container">
+			<div className="down">
+				<button onClick={remove} id="clear" className="item">Clear</button>
+				<button onClick={remove_one} id="del" className="item">DEL</button>
+				<button onClick={addChar} className="op item">/</button>
+				<button onClick={addChar} className="item">1</button>
 
-            <div className="up">
-                {Res}
-            </div>
+				<button onClick={addChar} className="item">2</button>
+				<button onClick={addChar} className="item">3</button>
+				<button onClick={addChar} className="op item">*</button>
+				<button onClick={addChar} className="item">4</button>
 
+				<button onClick={addChar} className="item">5</button>
+				<button onClick={addChar} className="item">6</button>
+				<button onClick={addChar} className="op item">-</button>
+				<button onClick={addChar} className="item">7</button>
 
-            <div className="down">  
-                <button onClick={remove} id="clear" className="item">Clear</button>
-                <button onClick={remove_one} id="del" className="item">DEL</button>
-                <button onClick={addChar} className="op item">/</button>
-                <button onClick={addChar} className="item">1</button>
+				<button onClick={addChar} className="item">8</button>
+				<button onClick={addChar} className="item">9</button>
+				<button onClick={addChar} className="op item">+</button>
+				<button onClick={addChar} className="item">0</button>
 
-                <button onClick={addChar} className="item">2</button>
-                <button onClick={addChar} className="item">3</button>
-                <button onClick={addChar} className="op item">*</button>
-                <button onClick={addChar} className="item">4</button>
+				<button onClick={addChar} className="item">.</button>
+				<button onClick={calculate} id="eqaul" className="item">=</button>
 
-                <button onClick={addChar} className="item">5</button>
-                <button onClick={addChar} className="item">6</button>
-                <button onClick={addChar} className="op item">-</button>
-                <button onClick={addChar} className="item">7</button>
+			</div>
 
-                <button onClick={addChar} className="item">8</button>
-                <button onClick={addChar} className="item">9</button>
-                <button onClick={addChar} className="op item">+</button>
-                <button onClick={addChar} className="item">0</button>
-
-                <button onClick={addChar} className="item">.</button>
-                <button onClick={calculate} id="eqaul" className="item">=</button>
-                            
-            </div>
-
-        </div>
-    );
+		</div>
+	);
 }
 
 export default Container;
